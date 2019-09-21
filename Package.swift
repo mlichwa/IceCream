88692b5f3c4a303a666e7b5e11193b6a1e44c78b
Michal Lichwa <michal@strangeflock.com>
2019-09-21 15:32:38 +0200

    swift version to 4.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
@@ -9,11 +9,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/realm/realm-cocoa.git", from: "3.18.0"),
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "3.16.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "IceCream",
             dependencies: ["RealmSwift"]),
