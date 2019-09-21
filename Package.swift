2405b63a5b148a5fa831ea5c1a724184659fe852
Michal Lichwa <michal@strangeflock.com>
2019-09-21 15:08:19 +0200

    Swift PM package file

---
+++ Package.swift
@@ -0,0 +1,19 @@
+import PackageDescription
+
+let package = Package(
+    name: "IceCeram",
+    products: [
+        .executable(name: "IceCream", targets: ["IceCream"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/mlichwa/IceCream.git", from: "1.11.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "IceCream",
+            dependencies: ["RealmSwift"]),
+    ]
+)
